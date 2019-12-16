67e88055e40699ad34c531b47cb22131830ad98d
Horváth Leó <horvath.leo2001@gmail.com>
2019-12-16 20:14:01 +0100

    work on models

--- salesgrade_framework/database/models.py
+++ salesgrade_framework/database/models.py
@@ -17,3 +17,50 @@ class Age(models.Model):
     user_age = models.IntegerField(default='N/I')
     def __str__(self):
         return str(self.user_age)
+
+#kpi 01
+class ProfessionCommunication(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    prof_comm_score = models.IntegerField(default=1)
+    prof_comm_status = models.IntegerField(default=1)
+
+#kpi 02
+class Teamwork(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    teamwork_score = models.IntegerField(default=1)
+    teamwork_status =  models.IntegerField(default=1)
+
+#kpi 03
+class ProductKnowledge(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    prod_knowledge_score =  models.IntegerField(default=1)
+    prod_knowledge_status =  models.IntegerField(default=1)
+    
+#kpi 04
+class PartnerCompetition(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    part_comp_score =  models.IntegerField(default=1)
+    part_comp_status =  models.IntegerField(default=1)
+
+#kpi 05
+class Relations(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    relations_score =  models.IntegerField(default=1)
+    relations_status =  models.IntegerField(default=1)
+
+#kpi 06
+class Strategy(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    strategy_score =  models.IntegerField(default=1)
+    Strategy_status =  models.IntegerField(default=1)
+
+#kpi 07
+class Loyality(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    loyality_score =  models.IntegerField(default=1)
+    loyality_status =  models.IntegerField(default=1)
+
+#overall
+class Overall(models.Model):
+    name = models.ForeignKey(Name, on_delete=models.CASCADE)
+    overall_score = models.IntegerField(default = 7)
